3889d7e23da108f2754d97d7ce42411913ebf5ca
leed2018 <dlee@sjnma.org>
2018-05-17 09:05:47 -0500

    index.html

---
+++ main.js
@@ -0,0 +1,31 @@
+alert("hello!!");
+console.log("i love dogs there kool")
+
+
+
+alert("welcome to mad libs");
+var person = prompt("Please enter your  name", "wombatman");
+var font = prompt("what is your favorite font?","Comic Sans");
+var noun1 = prompt("please enter a noun", "");
+var noun2 = prompt("please eanter a noun", "")
+var noun3 = prompt("please eanter a noun with an s", "")
+var noun4 = prompt("please eanter a noun", "")
+var verb1 = prompt("please eanter a verb", "")
+var verb2 = prompt("please eanter a verb with ing", "")
+var verb3 = prompt("please eanter a verb", "")
+var verb4 = prompt("please eanter a verb", "")
+var Adjective1 = prompt("please enter an adjective", "")
+var Adjective2 = prompt("please enter an adjective", "")
+
+
+
+
+
+
+
+
+
+
+document.getElementById("name").innerHTML = " there once was a " + noun1 + " he was walking in the park with a " + noun2 + " the " + noun2 + " had a couople " + noun3 + " he decied to " + verb1+ 
+" while he was " + verb1 + " there was a " + noun4 + " he had a very big bat and  was " + verb2 + " and " + noun4 + " was having a very good time doing his thing and sense he was havig such a good time he wanted to buy a cat and he he had a birthday party for the cat than the cat  " + verb3 + " while the cat was doing that the owner was " + verb4 + " but the cats fur was looking pretty " + Adjective1 + " and his owner was looking the same "
+ 
